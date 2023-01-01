30cf1f017a4613d8b8beaeced74a57aeaefb0336
Eduardo Goes <eduardo.paulog@gmail.com>
2023-01-01 10:11:59 -0300

    🐛 Fix FizzBuzz tests

--- ch2 - program structure/fizzBuzz/fizzBuzz.test.js
+++ ch2 - program structure/fizzBuzz/fizzBuzz.test.js
@@ -5,9 +5,9 @@ test("expect fizzbuzz to work for numbers divisible by 3, but not 5", () => {
 });
 
 test("expect fizzbuzz to work for numbers divisible by 5, but not 3", () => {
-    expect(fizzBuzz(3)).toBe("5 Buzz");
+    expect(fizzBuzz(5)).toBe("5 Buzz");
 });
 
 test("expect fizzbuzz to work for numbers divisible by 3 and 5", () => {
-    expect(fizzBuzz(3)).toBe("15 Fizz Buzz");
+    expect(fizzBuzz(15)).toBe("15 Fizz Buzz");
 });
