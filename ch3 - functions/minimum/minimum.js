5d906a1d4a245bd25909d68fecbb36e12662ce8c
Eduardo Goes <eduardo.paulog@gmail.com>
2023-01-01 10:51:28 -0300

    # This is a combination of 2 commits. # This is the 1st commit message:
    implement minimum function
    
    # This is the commit message #2:
    
    fix module exports

---
+++ ch3 - functions/minimum/minimum.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {Number} a First number
+ * @param {Number} b Second number
+ * @returns The smallest number between a and b
+ */
+const minimum = (a, b) => {
+    return (a <= b) ? a : b;
+}
+
+module.exports = minimum;
